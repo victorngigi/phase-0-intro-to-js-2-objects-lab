3201ae290555eeb42d9ff1f88eac4b5521ad411c
Victor Kinyanjui Ngigi <victor.ngigi@outlook.com>
2025-04-03 12:49:56 +0300

    final

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+let employee = {
+    name: "",
+    streetAddress: ""
+}
+updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    const newEmployee = {...employee}
+    newEmployee[key] = value
+    return newEmployee
+}
+destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value
+    return employee
+}
+deleteFromEmployeeByKey = (employee, key) => {
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key]
+    return employee
+}
